db00946238e6acc53d694a1672c84b95fd141ff2
Jan Kleprlík <kleprlikjan@gmail.com>
2022-08-22 21:32:05 +0200

    add formating bs

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -2,7 +2,11 @@ package com.demo;
 
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+      public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        var a = 10;
+
+
+      }
 }
